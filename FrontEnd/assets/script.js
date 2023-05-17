6c55b3fa8a4b4940c2319c5ea6891f200a486506
narsemjosue <narsemjosue2022@gmail.com>
2023-05-17 12:22:46 +0200

    modal et suppression

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,63 +1,156 @@
-const reponse = await fetch('http://localhost:5678/api/works');
+const reponse = await fetch("http://localhost:5678/api/works");
 const projets = await reponse.json();
 console.log(projets);
-function genererProjet(projets){
-for (let i = 0; i < projets.length; i++){
-    const figure =projets[i];
+function genererProjet(projets) {
+  for (let i = 0; i < projets.length; i++) {
+    const figure = projets[i];
     // Element accueillant
-    const sectionProjets = document.querySelector('.gallery');
-   
-    const projetElement = document.createElement('figure');
-    const imageElement = document.createElement('img');
-    imageElement.src = figure.imageUrl
-    const nomElement = document.createElement('figcation');
-    nomElement.innerText = figure.title
-  
+    const sectionProjets = document.querySelector(".gallery");
+
+    const projetElement = document.createElement("figure");
+    const imageElement = document.createElement("img");
+    imageElement.src = figure.imageUrl;
+    const nomElement = document.createElement("figcation");
+    nomElement.innerText = figure.title;
+
     sectionProjets.appendChild(projetElement);
     projetElement.appendChild(imageElement);
     projetElement.appendChild(nomElement);
-}
+  }
 }
 genererProjet(projets);
 
 // gestion des boutons
-const boutonTous = document.querySelector('.tous');
-boutonTous.addEventListener('click', function(){
-    const TousProjet = projets.filter(function(projet){
-        return projet;
-    });
-    document.querySelector('.gallery').innerHTML = "";
-    genererProjet(TousProjet);
-    console.log(TousProjet);
+const boutonTous = document.querySelector(".tous");
+boutonTous.addEventListener("click", function () {
+  const TousProjet = projets.filter(function (projet) {
+    return projet;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererProjet(TousProjet);
+  console.log(TousProjet);
 });
 
-const boutonObjet = document.querySelector('.objets');
-boutonObjet.addEventListener('click', function(){
-    const ObjetProjet = projets.filter(function(projet){
-        return projet.categoryId === 1;
-    });
-    document.querySelector('.gallery').innerHTML = "";
-    genererProjet(ObjetProjet);
-    console.log(ObjetProjet);
+const boutonObjet = document.querySelector(".objets");
+boutonObjet.addEventListener("click", function () {
+  const ObjetProjet = projets.filter(function (projet) {
+    return projet.categoryId === 1;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererProjet(ObjetProjet);
+  console.log(ObjetProjet);
 });
 
-const boutonAppartement = document.querySelector('.appart');
-boutonAppartement.addEventListener('click', function(){
-    const AppartProjet = projets.filter(function(projet){
-        return projet.categoryId === 2;
-    });
-    document.querySelector('.gallery').innerHTML = "";
-    genererProjet(AppartProjet);
-    console.log(AppartProjet);
+const boutonAppartement = document.querySelector(".appart");
+boutonAppartement.addEventListener("click", function () {
+  const AppartProjet = projets.filter(function (projet) {
+    return projet.categoryId === 2;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererProjet(AppartProjet);
+  console.log(AppartProjet);
 });
 
-const boutonHotel = document.querySelector('.hotel');
-boutonHotel.addEventListener('click', function(){
-    const hotelProjet = projets.filter(function(projet){
-        return projet.categoryId === 3;
-    });
-    document.querySelector('.gallery').innerHTML = "";
-    genererProjet(hotelProjet);
-    console.log(hotelProjet);
+const boutonHotel = document.querySelector(".hotel");
+boutonHotel.addEventListener("click", function () {
+  const hotelProjet = projets.filter(function (projet) {
+    return projet.categoryId === 3;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererProjet(hotelProjet);
+  console.log(hotelProjet);
 });
 
+const tok = window.localStorage.getItem("token");
+if (tok) {
+  console.log("test reussi");
+  document.querySelector(".login").innerText = "logOut";
+  let btn = document.querySelector(".btn");
+  btn.style.display = "none";
+} else {
+  console.log("test non reussi");
+  const btn = document.querySelectorAll(".hidden");
+  console.log(btn);
+   btn.classList.remove("hidden");
+  const edit = document.getElementById("edition");
+  edit.style.display = "none";
+}
+
+// const log = document.querySelector(".login");
+// log.addEventListener("click", function (){
+//   if(tok){
+//   localStorage.removeItem('token');
+//   document.location = '';
+// }
+// else{
+//   document.location = '/FrontEnd/login.html';
+// }
+// } );
+
+// GESTION DE LA MODAL****************
+
+var modal = document.getElementById("myModal");
+const btn_projet = document.getElementById("myBtn");
+// Contenu de la modal
+for (let i = 0; i < projets.length; i++) {
+  const figure = projets[i];
+  const sectionProjets = document.querySelector(".corps-modal");
+  const projetElement = document.createElement("figure");
+  const imageElement = document.createElement("img");
+  imageElement.src = figure.imageUrl;
+  const nomElement = document.createElement("figcation");
+  nomElement.innerText = "editer";
+  const iconesup = document.createElement("i");
+  iconesup.classList.add("fa-solid", "fa-trash-can");
+
+  sectionProjets.appendChild(projetElement);
+  projetElement.appendChild(imageElement);
+  projetElement.appendChild(nomElement);
+  projetElement.appendChild(iconesup);
+}
+const span = document.getElementsByClassName("close")[0];
+// Click sur le bouton de la modal modifier
+btn_projet.addEventListener("click", function () {
+  modal.style.display = "block";
+  //suppression d'un element
+  let btnsupprimer = document.querySelectorAll(".fa-trash-can");
+  console.log(btnsupprimer);
+  for (let i = 0; i < btnsupprimer.length; i++) {
+    btnsupprimer[i].addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(event);
+      let id_supprimer = projets[i].id;
+      console.log(id_supprimer);
+      let monToken = localStorage.getItem("token");
+      let response = fetch(`http://localhost:5678/api/works/${id_supprimer}`, {
+        method: "DELETE",
+        headers: {
+          accept: "*/*",
+          Authorization: `Bearer ${monToken}`,
+        },
+      });
+      if (response.status == 200) {
+
+        alert("suppression reussi")
+        return false;
+        // if HTTP-status is 200-299
+        //alert("Photo supprimé avec succes");
+        // obtenir le corps de réponse (la méthode expliquée ci-dessous)
+      } else {
+        alert("Echec de suppression");
+      }
+    });
+  }
+});
+span.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// GESTION DE LA MODAL DU BOUTON AJOUTER PHOTO
+const btn_ajoutphoto = document.querySelector(".ajoutphoto");
+btn_ajoutphoto.addEventListener("click", function(e){
+  e.preventDefault();
+  console.log('click reussi');
+  document.querySelector('.corps-modal').innerHTML = "";
+
+})
--- FrontEnd/assets/signIn.js
+++ FrontEnd/assets/signIn.js
@@ -1,153 +1,28 @@
-/*const $signInForm = document.querySelector('.signIn-form');
-
-
-const url = 'http://localhost:5678/api/users/login';
-
-
-const $userEmailErrorMsg = document.querySelector('.user-email-error-msg')
-const modif = document.querySelector('.btn-modif')
-
-const $userEmailInput = document.querySelector('#email')
-const $userPasswordInput = document.querySelector('#motdepasse')
-const USER_EMAIL = "thomas@facadia.com"
-const USER_PASSWORD = "azerty"
-
-const checkUserEmailInput = () => {
-    const isUserEmailValid = $userEmailInput.value.toLowerCase() === USER_EMAIL
-
-    if (isUserEmailValid) {
-       
-    }
-    else{
-       
-        alert('E-mail incorrect');
-    }
-
-    return isUserEmailValid
-}
-
-const checkUserPasswordInput = () => {
-    const isUserPasswordValid = $userPasswordInput.value === USER_PASSWORD
-
-    if (isUserPasswordValid) {
-        //$userPasswordErrorMsg.classList.add('hidden')
-    } 
-    else{
-        alert('Mot de passe incorrect');
-    }
-    return isUserPasswordValid
-}
-
-const isFormValid = () => checkUserEmailInput() && checkUserPasswordInput()
-
-$signInForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    if (isFormValid()) {
-        window.location = 'http://127.0.0.1:55/FrontEnd/index.html'
-        modif.nextElementSibling.getElementsByClassName.visbility = visible;
-    }
-  
-})*/////////////////////////////
-/*
-
-const userEmailInput = document.querySelector("#email").value;
-const userPasswordInput = document.querySelector("#motdepasse").value;
-
-let user = {
-    email :userEmailInput,
-    password :userPasswordInput
-};
-
-const emailCheck = document.getElementById("email");
-const passwordCheck = document.getElementById("motdepasse");
-
-emailCheck.reportValidity();
-passwordCheck.reportValidity();
-
-if(emailCheck.reportValidity() === false || passwordCheck.reportValidity() === false)
-{
-    return;
-}
-else{
-
-fetch('http://localhost:5678/api/users/login',
- {
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
-})
-
-.then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else if (res.status === 404) {
-      error.innerHTML = "Erreur dans l'identifiant";
-    } else if (res.status === 401) {
-      error.innerHTML = "Erreur dans le mot de passe";
-    }
-  })
-  .then((userLogged) => {
-    if (userLogged === undefined) {
-      return;
-
-      // If the user is logged in call the function
-    } else {
-      validateUser(userLogged);
-    }
-  })
-  .catch((err) => {
-    alert("erreur 404, problème avec le serveur:" + err);
-  });
-
-};
-
-// Function to validate the logged in user and redirect to the index page
-function validateUser(userLogged) {
-const userId = userLogged.userId;
-
-// Storage of the user token
-if (userId !== undefined) {
-localStorage.setItem("token", userLogged.token);
-localStorage.setItem("userId", userLogged.userId);
-document.location.href = "http://127.0.0.1:55/FrontEnd/index.html";
-} else {
-}
-console.log(userId);
-}
-*/
-
 document.querySelector("#btn-login").onclick = (e) => {
     e.preventDefault();
-  
-    // Get the error display message
-    //const error = document.querySelector("#error");
-  
-    // Get the informations of the user and create a user object
+    // recuperation des identifiants
     const emailAddress = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
-    // Create a user object
+    //utilisateur
     const user = {
       email: emailAddress,
       password: password,
     };
   
-    // Check the validity of the email and password inout
+    //test de validité
     const emailCheck = document.getElementById("email");
     const passwordCheck = document.getElementById("password");
     passwordCheck.reportValidity();
     emailCheck.reportValidity();
   
-    // If either the email or password is invalid return false
+    // returner false si email ou password est invalide
     if (
       emailCheck.checkValidity() === false ||
       passwordCheck.checkValidity() === false
     ) {
       alert("erreur");
   
-      // If both email and password are valid send a POST request to the API
+      // envoyer requête si identifiant valide
     } else {
       fetch("http://localhost:5678/api/users/login", {
         method: "POST",
@@ -156,7 +31,7 @@ document.querySelector("#btn-login").onclick = (e) => {
         },
         body: JSON.stringify(user),
       })
-        // If the response is OK, return the JSON data Else return error
+        // si la requete est OK, returner JSON data sinon erreur
         .then((res) => {
           if (res.ok) {
             return res.json();
@@ -184,15 +59,13 @@ document.querySelector("#btn-login").onclick = (e) => {
   // Function to validate the logged in user and redirect to the index page
   function validateUser(userLogged) {
     const userId = userLogged.userId;
-    const modif = document.querySelector('.btn-modif')
-   // const affich = modif.classList;
-    // Storage of the user token
+    // Stokage du token
     if (userId !== undefined) {
-      localStorage.setItem("token", userLogged.token);
-      localStorage.setItem("userId", userLogged.userId);
+      window.localStorage.setItem("token", userLogged.token);
+      window.localStorage.setItem("userId", userLogged.userId);
       document.location= "./index.html";
-     // affich.remplace("hidden", "visible");
     } else {
     }
     console.log(userId);
   }
+
